f4085f975a1df7b668444dce98103b37bb355fc7
Jesús Pelay <pelayjesus@gmail.com>
2021-03-15 23:45:56 -0400

    Added excerise 2

--- pa3/binSub/binSub.c
+++ pa3/binSub/binSub.c
@@ -13,7 +13,6 @@ int main(int argc, char *argv[]) {
     // SETUP
     
     // first, read the minuend (number to be subtracted from)
-    char buff;
     bool minuend[8]; // suggested that you store bits as array of bools; minuend[0] is the LSB
     for (int i=7; 0<=i; i--) { // read MSB first as that is what comes first in the file
         //Transforming the character read from stream to int and then to bool 
@@ -32,20 +31,41 @@ int main(int argc, char *argv[]) {
 
     // Negate the subtrahend
     // flip all bits
-    /* ... */
-
+    for(int i=7;0<=i;i--) {
+        subtrahend[i] = !subtrahend[i];
+    }
     // add one
     bool carry = true; // to implement the 'add one' logic, we do binary addition logic with carry set to true at the beginning
-    for (int i=0; i<8; i++) { // iterate from LSB to MSB
-        /* ... */
-    }
-    // Add the minuend and the negated subtrahend
     bool difference[8];
-    /* ... */
-
+    int result = 0;
+    for (int i=0; i<8; i++) { // iterate from LSB to MSB
+        result = minuend[i] + subtrahend[i] + carry;
+        printf("%d %d %d %d\n",minuend[i],subtrahend[i],carry,result);
+        if(result == 0){
+            difference[i] = false;
+            carry = false;
+        }
+        else if (result == 1) {
+            difference[i] = true;
+            carry = false;
+        } else if (result == 2) {
+            difference[i] = false;
+            carry = true;
+        } else{
+            difference[i] = true;
+            carry = true;
+        }
+    }
     // print the difference bit string
     for (int i=7; 0<=i; i--)
+        printf("%d",minuend[i]);
+    printf("\n");
+    for (int i=7; 0<=i; i--)
         printf("%d",subtrahend[i]);
+    printf("\n");
+    for (int i=7; 0<=i; i--)
+        printf("%d",difference[i]);
+    
 
     return EXIT_SUCCESS;
 
