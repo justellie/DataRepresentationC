9ebca3c2d66ee199850717726343103a607c6e81
Elio <Elioruiz.en@gmail.com>
2021-03-16 20:47:25 -0400

    doubleToBin corre casi completo

--- pa3/doubleToBin/doubleToBin.c
+++ pa3/doubleToBin/doubleToBin.c
@@ -11,16 +11,7 @@
 int main(int argc, char *argv[]) {
 
     // SETUP
-    int sign;
-    int intpart = 0;
-    int decimal[18];
-    int exponent[11];
-    int frac[52];
-    int countE=17,countF=0,countZeros=0;
-    int e=0;
-    int pres=52;
-    int i=0;
-    double decpart = 0;
+
     FILE* fp = fopen(argv[1], "r");
     if (!fp) {
         perror("fopen failed");
@@ -30,145 +21,100 @@ int main(int argc, char *argv[]) {
     // first, read the number
     double value;
     fscanf(fp, "%lf", &value);
-    sign = value<0.0;
-    
+
     // the reference solution ('the easy way')
     // you are not allowed to print from this casted 'ref_bits' variable below
     // but, it is helpful for validating your solution
     unsigned long int ref_bits = *(unsigned long int*) &value;
 
-     
     // THE SIGN BIT
-    printf("%d_", sign);
-    assert ( sign == (1&ref_bits>>(EXP_SZ+FRAC_SZ))); // validate your result against the reference
-
+    bool sign = value<0.0;
+    printf("%d_",sign);
+    assert (sign == (1&ref_bits>>(EXP_SZ+FRAC_SZ))); // validate your result against the reference
+    
     // THE EXP FIELD
     // find the exponent E such that the fraction will be a canonical fraction:
     // 1.0 <= fraction < 2.0
-    //111010
-
-    //init
-    value=sign ? -value : value;
-    intpart = (int)value;
-    decpart = value- intpart;
-    for( i=0;i<18;i++)    
-    {    
-        decimal[i]=-1;
-        frac[i]=-1;   
-         
-    }
-    i=0;
-    for(int i=0;i<52;i++)    
-    {    
-        frac[i]=-1;   
-    }
-    i=0;
-    for (int i = 0; i < 11; i++)
+    double fraction = sign ? -value : value;
+    double nf=fraction;
+    int trial_exp=(1<<(EXP_SZ-1)); // start by assuming largest possible exp (before infinity)
+    // do trial division until the proper exponent E is found
+    /* ... */
+    unsigned short bias = (1<<(EXP_SZ-1))-1;
+    signed short exp=0;
+    if(fraction>0)
     {
-        exponent[i] = 0;
-    }
-
-    //treatment of the number
-    int n=intpart;
-    i=0;
-    for( i=0;n>0;i++)    
-    {    
-        decimal[i]=n%2;    
-        n=n/2;
-    }
-
-    double nf=decpart;
-    while (pres--) 
-    { 
-        // Find next bit in fraction 
-        nf *= 2; 
-        long fract_bit = nf; 
-  
-        if (fract_bit == 1) 
-        { 
-            nf -= fract_bit; 
-            frac[countF]=1;
+        //https://www.tutorialspoint.com/c_standard_library/c_function_frexp.htm
+        frexp(fraction,&trial_exp);
             
-        } 
-        else
-        {
-            frac[countF]=0;
-        }
-        countF++; 
-    } 
-
-    //in case =0
-    if (intpart != 0)
-    {
-        while (decimal[countE] != 1)
-        {
-            countE--;
-        }
-        e = 1023 + countE;
+        exp = (trial_exp-1) + bias;
     }
-    else
-    {
-        countF = 0;
-        if (decpart != 0)
-        {
-            while (frac[countF] != 1)
-            {
-                countF++;
-            }
-            e = 1023 -(countF+1);
-        }
-    }
-    i=0;
-    for (i = 0; e > 0; i++)
-    {
-        exponent[i] = e % 2;
-        e = e / 2;
-    }
-    i=10;
-    for(i=10;i>-1;i--)    
-    {   
-        printf("%d",exponent[i]);
+    
+    for ( int exp_index=EXP_SZ-1; 0<=exp_index; exp_index-- ) {
+        bool exp_bit = 1&exp>>exp_index;
+        printf("%d",exp_bit);
+        //assert (exp_bit == (1&ref_bits>>(exp_index+FRAC_SZ))); // validate your result against the reference
     }
     printf("_");
+    // you get partial credit by getting the exp field correct
+    // even if you print zeros for the frac field next
+    // you should be able to pass test cases 0, 1, 2, 3
 
-    //in number >.0
-    if (intpart > 0)
-    {   
-        i=countE-1;
-        for (i = countE-1; i > -1; i--)
+    // THE FRAC FIELD
+    // prepare the canonical fraction such that:
+    // 1.0 <= fraction < 2.0
+    /* ... */
+    int fract_bit;
+    nf = fraction;
+    if (fraction >= 0.99999)
+    {
+        while (nf >= 2 && !(nf <= 1))
         {
-            printf("%d",decimal[i]);
-            countZeros++;
+            nf = nf / 2;
         }
-        if(frac[0]!=-1)
+        if(nf==1)
         {
-            i=0;
-            for (i = 0; i < (52-countZeros); i++)
-            {
-                printf("%d", frac[i]);
-                
-            }
-            countZeros+=i;
+            nf = 0;
         }
     }
     else
     {
-        if (decpart != 0)
+        frexp(fraction, &trial_exp);
+        trial_exp--;
+        int i = 0;
+        while (i > trial_exp)
         {
-            i=countF+1;
-            for (i = countF+1; i < (52-countZeros); i++)
+            nf *= 2;
+            fract_bit = nf;
+            if (fract_bit)
             {
-                printf("%d", frac[i]);
-                
+                nf -= fract_bit;
             }
-            countZeros+=i-(countF+1);
+            i--;
         }
+        exp = 0;
     }
-    i=countZeros;
-    for (i = countZeros; i < 52; i++)
-    {
-         printf("%d", 0);
+
+
+    bool frac_array[FRAC_SZ+1]; // one extra LSB bit for rounding
+    for ( int frac_index=FRAC_SZ; 0<=frac_index; frac_index-- ) {
+        
+        nf *= 2;
+        fract_bit = nf; 
+        frac_array[frac_index] = fract_bit; // frac set to zero to enable partial credit
+        if (fract_bit)
+        {
+            nf -= fract_bit;
+        }   
     }
-    //printf("\n");
-    
+
+    // rounding
+    /* ... */
+
+    for ( int frac_index=FRAC_SZ-1; 0<=frac_index; frac_index-- ) {
+        bool frac_bit = frac_array[frac_index+1]; // skipping the extra LSB bit for rounding
+        printf("%d", frac_bit);
+        // assert (frac_bit == (1&ref_bits>>frac_index)); // validate your result against the reference
+    }
+
 }
